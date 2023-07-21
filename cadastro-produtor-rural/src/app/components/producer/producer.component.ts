1b2ffc56514bb297c2901e6361174102dbd3d863
Thalita <thalita.andradersantos@gmail.com>
2023-07-21 20:28:53 -0300

    Subindo atualização da branch

--- cadastro-produtor-rural/src/app/components/producer/producer.component.ts
+++ cadastro-produtor-rural/src/app/components/producer/producer.component.ts
@@ -7,8 +7,6 @@ import { Register } from 'src/app/interfaces/register';
   styleUrls: ['./producer.component.scss']
 })
 export class ProducerComponent implements OnInit {
-  plantedCropsValue!: string[]
-
   @Input() register: Register = {
     id: 0,
     cpfCnpjField: '',
@@ -25,40 +23,6 @@ export class ProducerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.plantedCropsValue = [];
-    for (let index = 0; index < this.register.plantedCrops.length; index++) {
-      const element = this.register.plantedCrops[index];
-      switch (index) {
-        case 0:
-            if(element){
-              this.plantedCropsValue.push('Milho')
-            }
-          break;
-        case 1:
-            if(element){
-              this.plantedCropsValue.push('Soja')
-            }
-          break;
-        case 2:
-            if(element){
-              this.plantedCropsValue.push('Café')
-            }
-          break;
-        case 3:
-            if(element){
-              this.plantedCropsValue.push('Algodão')
-            }
-          break;
-        case 4:
-            if(element){
-              this.plantedCropsValue.push('Cana de Açucar')
-            }
-          break;
-
-        default:
-          break;
-      }
-    }
   }
 
 }
